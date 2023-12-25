ca621d5ef1ee1388b693b40e35fe0101450b1c06
nesirli <nasir.nesirli@gmail.com>
2023-12-25 22:21:19 +0400

    Two Sum problem is solved.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import easy.TwoSum;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/easy/TwoSum.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] solution = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] + nums[j] == target) {
+                    solution[0] = i;
+                    solution[1] = j;
+                }
+            }
+        }
+        return solution;
+    }
+}
